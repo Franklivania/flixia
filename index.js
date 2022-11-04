348b2ffd7ba268a29f0b08ca159444d78e8962a1
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2022-11-04 17:00:59 +0100

    completed form, added icons

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ tabButtons.forEach(btn => {
     
     })
 })
+
+
+document.querySelector("#show-login").addEventListener("click", function (){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
